79b420631cdf9752505eb9ab2cd874d89797efe0
Laura Radville <laura.radville@gmail.com>
2019-07-17 13:05:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,9 +36,10 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, season_hash|
     puts season.to_s.capitalize! + ":"
     season_hash.each do |holiday, supplies|
+      holiday_array = []
       holiday.to_s.split.each do |word|
-        array = word.capitalize!
-        
+        holiday_array << word.capitalize!
+        binding.pry
 
       end
       
