46dd5dcb939213d286bcd246d3ab41a0b3ed7d2d
Laura Radville <laura.radville@gmail.com>
2019-07-17 12:50:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -33,7 +33,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each |
+  holiday_hash.each |season, season_hash|
+  binding.pry
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
